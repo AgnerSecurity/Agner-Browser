03f1d5d620579c1c1fd12b78a18948d18dded255
Luis Carlos Agner Claro - Agner Security <63209606+AgnerSecurity@users.noreply.github.com>
2021-08-31 22:16:51 -0300

    Add files via upload

---
+++ Código em Python PT-BR/main.py
@@ -0,0 +1,54 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+
+class MainWindow(QMainWindow):
+   def __init__(self):
+       super(MainWindow, self).__init__()
+       self.browser = QWebEngineView()
+       self.browser.setUrl(QUrl('http://google.com'))
+       self.setCentralWidget(self.browser)
+       self.showMaximized()
+
+       navbar = QToolBar()
+       self.addToolBar(navbar)
+
+       back_btn = QAction('Voltar', self)
+       back_btn.triggered.connect(self.browser.back)
+       navbar.addAction(back_btn)
+
+       forward_btn = QAction('Avançar', self)
+       forward_btn.triggered.connect(self.browser.forward)
+       navbar.addAction(forward_btn)
+
+       reload_btn = QAction('Recarregar', self)
+       reload_btn.triggered.connect(self.browser.reload)
+       navbar.addAction(reload_btn)
+
+       home_btn = QAction('Home', self)
+       home_btn.triggered.connect(self.navigate_home)
+       navbar.addAction(home_btn)
+
+       self.url_bar = QLineEdit()
+       self.url_bar.returnPressed.connect(self.navigate_to_url)
+       navbar.addWidget(self.url_bar)
+
+       self.browser.urlChanged.connect(self.update_url)
+
+   def navigate_home(self):
+       self.browser.setUrl(QUrl('http://baidu.com'))
+
+   def navigate_to_url(self):
+       url = self.url_bar.text()
+       self.browser.setUrl(QUrl(url))
+
+   def update_url(self, q):
+       self.url_bar.setText(q.toString())
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName('Agner Browser')
+window = MainWindow()
+app.exec_()
+
+
