d25e258a6767c60ae1844089bdd89ca5bc7cf0e3
sirLobardo <61591862+sirLobardo@users.noreply.github.com>
2021-09-08 09:50:31 -0300

    change baidu homeurl to duckduckgo

--- Código em Python PT-BR/main.py
+++ Código em Python PT-BR/main.py
@@ -37,7 +37,7 @@ class MainWindow(QMainWindow):
        self.browser.urlChanged.connect(self.update_url)
 
    def navigate_home(self):
-       self.browser.setUrl(QUrl('http://baidu.com'))
+       self.browser.setUrl(QUrl('https://duckduckgo.com/'))
 
    def navigate_to_url(self):
        url = self.url_bar.text()
